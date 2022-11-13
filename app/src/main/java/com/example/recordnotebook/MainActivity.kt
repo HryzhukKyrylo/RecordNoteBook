da9f148b704fece284caa555696ab6de70c9617f
Kiri <kaktyss121@gmail.com>
2022-11-13 12:12:20 +0200

    Add action bar with navigate

--- app/src/main/java/com/example/recordnotebook/MainActivity.kt
+++ app/src/main/java/com/example/recordnotebook/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.recordnotebook
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+                    as NavHostFragment ?: return
+        val navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBar(navController, appBarConfiguration)
+    }
+
+    private fun setupActionBar(
+        navController: NavController,
+        appBarConfiguration: AppBarConfiguration
+    ) {
+        setupActionBarWithNavController(
+            navController = navController,
+            configuration = appBarConfiguration
+        )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.fragmentContainerView).navigateUp(appBarConfiguration)
     }
 }
